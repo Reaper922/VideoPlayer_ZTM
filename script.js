cecedb0641485b8ed76289ea129f5a4eb06f9ffe
Dennis Ammen <dennis.ammen@outlook.com>
2022-06-29 23:56:15 +0200

    Poplate current time / duration

--- script.js
+++ script.js
@@ -31,8 +31,17 @@ video.addEventListener('ended', showPlayIcon);
 
 
 // Progress Bar ---------------------------------- //
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+
 function updateProgress() {
-    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
 }
 
 
